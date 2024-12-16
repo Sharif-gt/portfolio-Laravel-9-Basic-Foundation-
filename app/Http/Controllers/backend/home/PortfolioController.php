9c24603c8b68f7b0631cd3ba77592fc2cc5bd698
Sharif-gt <shahidulislamsharif.com@gmail.com>
2024-12-16 12:22:26 +0600

    Portfoliop data add.

--- app/Http/Controllers/backend/home/PortfolioController.php
+++ app/Http/Controllers/backend/home/PortfolioController.php
@@ -5,10 +5,33 @@ namespace App\Http\Controllers\backend\home;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\admin\Portfolio;
+use Image;
 
 class PortfolioController extends Controller
 {
     public function index (){
         return view('backend.pages.portfolio.add-portfolio');
+    }
+
+    public function add (Request $request){
+        $request->validate([
+            'name' => 'required',
+            'title' => 'required',
+            'long_description' => 'required',
+        ]);
+
+        $image = $request->file('image');
+        $name_img = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->resize(1020,519)->save('upload/portfolio/'.$name_img);
+        $save_img = 'upload/portfolio/'.$name_img;
+
+        Portfolio::insert([
+            'name'=> $request->name,
+            'title'=> $request->title,
+            'long_description'=> $request->long_description,
+            'image'=> $save_img,
+        ]);
+        session()->flash('message','Portfolio Add Successfully.');
+        return redirect()->back();
     }
 }
--- resources/views/backend/pages/portfolio/add-portfolio.blade.php
+++ resources/views/backend/pages/portfolio/add-portfolio.blade.php
@@ -5,7 +5,7 @@
     <div class="col-xl-6">
         <div class="card">
             <div class="card-body">
-                <h4 class="card-title">Edit Your About</h4>
+                <h4 class="card-title">Add Portfolio</h4>
                 
                 <form class="" method="POST" action="{{route('portfolio.add')}}" enctype="multipart/form-data">
                      @csrf
@@ -15,29 +15,38 @@
                        <!-- Name -->
                         <div class="col-md-12">
                             <div class="mb-3 position-relative">
-                                <label for="validationTooltip01" class="form-label">Name</label>
+                                <label for="validationTooltip01" class="form-label">Name :</label>
                                 <input type="text" class="form-control" id="validationTooltip01" name="name" value="">
                             </div>
+                            @error('name')
+                                <span class="alert-danger">{{$message}}</span>
+                            @enderror
                         </div>
                         <!-- title -->
                         <div class="col-md-12">
                             <div class="mb-3 position-relative">
-                                <label for="validationTooltip01" class="form-label">Title</label>
+                                <label for="validationTooltip01" class="form-label">Title :</label>
                                 <input type="text" class="form-control" id="validationTooltip01" name="title" value="">
                             </div>
+                            @error('title')
+                                <span class="alert-danger">{{$message}}</span>
+                            @enderror
                         </div>
                         <!-- Long Description long_description -->
                        <div class="col-md-12 m-3">
                             <div class="mb-3 position-relative">
-                                <label for="validationTooltip02" class="form-label">Long Description</label>
+                                <label for="validationTooltip02" class="form-label">Long Description :</label>
                                <textarea id="elm1" name="long_description"></textarea>
                             </div>
+                            @error('long_description')
+                               <span class="alert-danger">{{$message}}</span>
+                            @enderror
                         </div>
                     <div class="row">
                         <!-- image -->
                         <div class="col-md-6">
                             <div class="mb-3 position-relative">
-                                <label for="validationTooltip02" class="form-label">Image</label>
+                                <label for="validationTooltip02" class="form-label">Image :</label>
                                 <input type="file" name="image" class="form-control" id="profile_image" value="" >
                             </div>
                         </div>
