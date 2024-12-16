c37c45d25f2185a0deddcfd2f37659a3c6fef1b6
Sharif-gt <shahidulislamsharif.com@gmail.com>
2024-12-16 11:06:04 +0600

    Display about page multi image

--- resources/views/frontend/home/about.blade.php
+++ resources/views/frontend/home/about.blade.php
@@ -1,6 +1,10 @@
 @extends('frontend.index_master')
 @section('index')
 
+@php
+    $images = App\Models\admin\MultiImage::get()->all();
+@endphp
+
 <!-- breadcrumb-area -->
 <section class="breadcrumb__wrap">
     <div class="container custom-container">
@@ -20,12 +24,11 @@
     </div>
     <div class="breadcrumb__wrap__icon">
         <ul>
-            <li><img src="{{ asset('frontend/assets/img/icons/breadcrumb_icon01.png')}}" alt=""></li>
-            <li><img src="{{ asset('frontend/assets/img/icons/breadcrumb_icon02.png')}}" alt=""></li>
-            <li><img src="{{ asset('frontend/assets/img/icons/breadcrumb_icon03.png')}}" alt=""></li>
-            <li><img src="{{ asset('frontend/assets/img/icons/breadcrumb_icon04.png')}}" alt=""></li>
-            <li><img src="{{ asset('frontend/assets/img/icons/breadcrumb_icon05.png')}}" alt=""></li>
-            <li><img src="{{ asset('frontend/assets/img/icons/breadcrumb_icon06.png')}}" alt=""></li>
+            @foreach ($images as $item)
+                
+            <li><img src="{{ asset($item->images)}}" alt=""></li>
+
+            @endforeach
         </ul>
     </div>
 </section>
